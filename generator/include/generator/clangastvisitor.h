d22bfa84b7e361a09380cb5ca495f676cf248768
nalevi <woundless17@gmail.com>
2021-01-14 19:35:54 +0100

    Ast visitor implementálás kezdete

---
+++ generator/include/generator/clangastvisitor.h
@@ -0,0 +1,62 @@
+#ifndef UMLGEN_CLANGASTVISITOR_H
+#define UMLGEN_CLANGASTVISITOR_H
+
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendAction.h>
+#include <clang/Tooling/Tooling.h>
+#include <iostream>
+
+namespace umlgen
+{
+namespace generator
+{
+
+class FindNamedClassVisitor
+  : public clang::RecursiveASTVisitor<FindNamedClassVisitor> {
+public:
+  explicit FindNamedClassVisitor(clang::ASTContext *Context)
+    : Context(Context) {}
+
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *Declaration) { 
+    std::cout << "Found class: "
+              << Declaration->getQualifiedNameAsString()
+              << std::endl;
+
+    std::cout << std::endl; 
+    return true;
+    }
+
+  bool VisitCXXMethodDecl(clang::CXXMethodDecl *dcl_) {
+    clang::CXXRecordDecl *cppClass = dcl_->getParent();
+    std::cout << "Found CXXMethod!" << std::endl;
+
+    if(dcl_->isVirtual()) {
+      std::cout << "Virtual method!" << std::endl;
+    }
+
+    std::cout << "Parent class: "
+              << cppClass->getNameAsString() 
+              << std::endl; 
+
+    std::cout << "Name: "
+              << dcl_->getNameInfo().getAsString()
+              << std::endl;
+
+    std::cout << "Return type: "
+              << dcl_->getReturnType().getAsString()
+              << std::endl;
+    
+    std::cout << std::endl;
+    return true;
+  }
+
+
+private:
+  clang::ASTContext *Context;
+};
+
+} //generator
+} //umlgen
+
+#endif
