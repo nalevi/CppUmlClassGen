3ee19be6d2fd5a4bdb9134adc2c554fb98e6bb48
nalevi <woundless17@gmail.com>
2021-02-26 15:27:07 +0100

    Adding command line options + util module
    Added boost::options_description. It doesn't work with clang's command line
    argument parsers (TODO). Added util modul for diagramm output. New error:
    If no namespace is decalred in the c++ file, then we get sqefault...
    Need to check in ASTVisitor everytime...

--- generator/src/generator.cpp
+++ generator/src/generator.cpp
@@ -1,3 +1,4 @@
+#include <boost/program_options/options_description.hpp>
 #include <clang/Frontend/FrontendAction.h>
 #include <iostream>
 #include <sstream>
@@ -17,7 +18,8 @@
 
 #include "clangastvisitor.h" 
 
-//namespace fs = boost::filesystem;
+namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 
 class ClangASTConsumer : public clang::ASTConsumer 
 {
@@ -81,13 +83,79 @@ static llvm::cl::extrahelp CommonHelp(
 static llvm::cl::extrahelp MoreHelp("\nMore help text...\n");
 
 
+po::options_description commandLineArgs()
+{
+  po::options_description desc("Generator options");
+
+  desc.add_options()
+    ("help,h",
+      "Prints help message.")
+    ("object,c", po::value<std::string>()->required(),
+      "The name of the object to generate uml class diagramm for.")
+    ("workspace,w", po::value<std::string>()->required(),
+      "The path to the project root library.")
+    ("fileformat,f",
+      "The output file format: SVG,TXT, DOT")
+    ("database,d",po::value<std::string>()->required(),
+      "The database used to store the AST informations,"
+      "valid options are PostgreSQL or SQLite. For PostgreSQl"
+      "a connection string is needed, for example: "
+      "'host=127.0.0.1 user=test password=1234 port5432 dbname=umlgen_test'");
+
+    return desc;
+}
+
+
+/**
+ * This function checks the existence of the workspace and project directory
+ * based on the given command line arguments.
+ * @return Whether the project directory exists or not.
+ */
+bool checkProjectDir(const po::variables_map& vm_)
+{
+  const std::string projDir
+    = vm_["workspace"].as<std::string>() + '/';
+
+  return fs::is_directory(projDir);
+}
+  
+
 int main(int argc, const char** argv)
 {
+
+  // Proecessing command line arguments
+  po::options_description desc = commandLineArgs();
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+    .options(desc).allow_unregistered().run(), vm);
+
+  std::string CONNECTION_STRING("");
+  std::string UML_CLASS("");
+
+  if(vm.count("workspace"))
+  {
+    if(!checkProjectDir(vm))
+    {
+      std::cerr << "Error: workspace doesn't exist!" << std::endl;
+    }
+  }
+
+  if(vm.count("database"))
+  {
+    CONNECTION_STRING = vm["database"].as<std::string>(); 
+  }
+
+  if(vm.count("object"))
+  {
+    UML_CLASS = vm["object"].as<std::string>();
+  }
+
+
   std::shared_ptr<Wt::Dbo::Session> session_ptr(new Wt::Dbo::Session);
 
-  // TODO: get the connection string from commandline argument
   bool dbSession = umlgen::generator::startDbSession(
     //"host=127.0.0.1 user=test password=1234 port=5432 dbname=umlgen_test",
+    //CONNECTION_STRING,
     "umlgen.db",
     session_ptr);
  
